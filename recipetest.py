61fabacc25897fe90dae0839f83bb92596f1c90c
Andrew <cartonandrew@gmail.com>
2018-08-03 13:36:09 +0100

    fixed tests with new database code so it would reflect the single connection per route

--- recipetest.py
+++ recipetest.py
@@ -1,24 +1,25 @@
 from byotest import *
 from recipemain import *
 
+conn = engine.connect()
 # delete the entire database and test
-db_delete()
-test_are_equal(user_count() == 0, True)
-test_are_equal(recipe_count() == 0, True)
-test_are_equal(ingredient_count() == 0, True)
+db_delete(conn)
+test_are_equal(user_count(conn) == 0, True)
+test_are_equal(recipe_count(conn) == 0, True)
+test_are_equal(ingredient_count(conn) == 0, True)
 
 # test the database functions for user
-test_are_equal(user_register("mytestuser", "mypass"), True)
-test_are_equal(user_authenticate("mybaduser", "mypass"), False)
-test_are_equal(user_authenticate("mytestuser", "mypass"), True)
-test_are_equal(user_authenticate("mytestuser", "badpass"), False)
-user_delete("mytestuser")
-test_are_equal(user_authenticate("mytestuser", "mypass"), False)
+test_are_equal(user_register("mytestuser", "mypass", conn), True)
+test_are_equal(user_authenticate("mybaduser", "mypass", conn), False)
+test_are_equal(user_authenticate("mytestuser", "mypass", conn), True)
+test_are_equal(user_authenticate("mytestuser", "badpass", conn), False)
+user_delete("mytestuser", conn)
+test_are_equal(user_authenticate("mytestuser", "mypass", conn), False)
 
 
 # Test insert recipe
 author = "mytestuser"
-user_register(author, "mypass")
+user_register(author, "mypass", conn)
 
 # Make a recipe
 name = "Pancakes"
@@ -51,17 +52,17 @@ direction2 = dict()
 direction2['direction'] = "To make the pancakes, heat a small heavy-based frying until very hot and then turn the heat down to medium. Lightly grease with oil and then ladle in enough batter to coat the base of the pan thinly (about 2 tablsp.), tilting the pan so the mixture spreads evenly. Cook over a moderate heat for 1-2 minutes or until the batter looks dry on the top and begins to brown at the edges. Flip the pancake over with a palette knife or fish slice and cook the second side."
 directions.append(direction2)
 
-recipeid = recipe_insert(name, author, country, course, ingreds, directions)
+recipeid = recipe_insert(name, author, country, course, ingreds, directions, conn)
 
 # Test everything was inserted in correctly
-test_are_equal(user_count() == 1, True)
-test_are_equal(recipe_count() == 1, True)
-test_are_equal(ingredient_count() == 3, True)
+test_are_equal(user_count(conn) == 1, True)
+test_are_equal(recipe_count(conn) == 1, True)
+test_are_equal(ingredient_count(conn) == 3, True)
 test_are_equal(recipeid == 1, True)
 
 # Retrieve the recipe from the database now
 # should return a recipe object
-recipe = recipe_get(recipeid)
+recipe = recipe_get(recipeid, conn)
 test_are_equal(recipe.id == 1, True)
 test_are_equal(recipe.name == "Pancakes", True)
 test_are_equal(recipe.country == "Greece", True)
@@ -104,3 +105,5 @@ test_are_equal(not ingr == "", True)
 test_are_equal(i.name == "milk", True)
 test_are_equal(i.amount == "200ml", True)
 test_are_equal(i.allergen == "dairy", True)
+
+conn.close()
