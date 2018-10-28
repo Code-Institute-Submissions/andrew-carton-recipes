626280bfeabe1c555654ed54d10cc50b64dc3475
Andrew Carton <andrew.t.carton@gmail.com>
2018-10-28 15:46:17 +0000

    Moved javascript in html files of insert recipe and list recipe pages to seperate javascript files in static folder

---
+++ static/js/insertrecipe.js
@@ -0,0 +1,75 @@
+var ingnum = 1;
+        var ingrlist = [];
+
+        function send() {
+            var form_data = new FormData($('#upload-file')[0]);
+            var x = form_data.get('file');
+
+            $.ajax({
+                url: "/uploadajax",
+                type: "POST",
+                data: form_data,
+                processData: false,
+                contentType: false,
+            });
+            var id = document.getElementById("myimage");
+            id.innerHTML = '<img src="/static/images/' + x.name + '" />';
+        }
+
+        function addIngredient() {
+            var id = document.getElementById("ingredient");
+            var aid = document.getElementById("allergen");
+            var tid = document.getElementById("ingredient_table");
+            var amid = document.getElementById("amount");
+
+
+            var ingobj = new Object();
+            ingobj.ingredient = id.value;
+            ingobj.allergen = aid.value;
+            ingobj.amount = amid.value;
+            ingrlist = ingrlist.concat(ingobj);
+            tid.innerHTML += "<tr><td>" + ingnum++ + "</td><td>" + id.value + "</td><td>" + amid.value + "</td><td>" + aid.value + "</td></tr>";
+            id.value = '';
+            aid.value = '';
+            amid.value = '';
+        }
+
+        var dirnum = 1;
+        var dirlist = [];
+
+        function addDirection() {
+            var id = document.getElementById("direction");
+
+            var tid = document.getElementById("directions_table");
+            tid.innerHTML += "<tr><td>" + dirnum++ + "</td><td>" + id.value + "</td></tr>";
+            var dirobj = new Object();
+            dirobj.direction = id.value;
+            dirlist = dirlist.concat(dirobj);
+            id.value = '';
+
+        }
+
+        function submit() {
+
+            var ingrObj = new Object();
+
+            ingrObj.name = document.getElementById("recipename").value;
+            ingrObj.country = document.getElementById("country").value;
+            ingrObj.course = document.getElementById("course").value;
+            ingrObj.author = document.getElementById("author").value;
+            ingrObj.ingredients = ingrlist;
+            ingrObj.directions = dirlist;
+            var form_data = new FormData($('#upload-file')[0]);
+            var x = form_data.get('file');
+
+            ingrObj.image = x.name
+
+            var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+            xmlhttp.open("POST", "/insertrecipe");
+            xmlhttp.setRequestHeader("Content-Type", "application/json");
+            xmlhttp.send(JSON.stringify(ingrObj));
+
+            var x = document.getElementById("fullrecipe");
+            x.innerHTML = "<h5>Thank you for submitting the " + ingrObj.name + " recipe!"
+
+        }
---
+++ static/js/listrecipes.js
@@ -0,0 +1,121 @@
+function searchByAllergen() {
+    var allergenid = document.getElementById("allergen");
+    if (allergenid.value == '') {
+        window.location.replace("listrecipes");
+        return;
+    }
+    var sstr = "/searchexcludeallergen?allergen=";
+
+    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+    sstr += allergenid.value;
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            var recipesdiv = document.getElementById("recipes");
+            var str = '<div class="row">'
+
+
+            for (var i = 0; i < data.length; i++) {
+                str += '<div class="col-sm-4 mb-3">';
+                str += '<div class="card" style="width: 18rem;">';
+                str += '<img class="card-img-top" src="static/images/';
+                str += '' + data[i].image + '" height="20%" width="20%">';
+                str += '<div class="card-body">';
+                str += ' <h5 class="card-title">' + data[i].name + '</h5>';
+                str += '<p class="card-text">A ';
+                str += '' + data[i].course + ' ' + data[i].country + ' dish ' + ' </p>';
+                str += ' <a href="recipe?id=' + data[i].id + '" class="btn btn-primary">View Recipe</a>';
+                str += '</div>';
+                str += '</div>';
+                str += '</div>';
+
+            }
+            str += "</div>";
+            recipesdiv.innerHTML = str;
+        }
+    };
+    xmlhttp.open("GET", sstr, true);
+    xmlhttp.send();
+}
+
+function searchByIngredient() {
+    var ingredientid = document.getElementById("ingredient");
+    if (ingredientid.value == '') {
+        window.location.replace("listrecipes");
+        return;
+    }
+    var sstr = "/searchbyingredient?ingredient=";
+
+    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+    sstr += ingredientid.value;
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            var recipesdiv = document.getElementById("recipes");
+
+            var str = '<div class="row">'
+
+
+            for (var i = 0; i < data.length; i++) {
+                str += '<div class="col-sm-4 mb-3">';
+                str += '<div class="card" style="width: 18rem;">';
+                str += '<img class="card-img-top" src="static/images/';
+                str += '' + data[i].image + '" height="20%" width="20%">';
+                str += '<div class="card-body">';
+                str += ' <h5 class="card-title">' + data[i].name + '</h5>';
+                str += '<p class="card-text">A ';
+                str += '' + data[i].course + ' ' + data[i].country + ' dish ' + ' </p>';
+                str += ' <a href="recipe?id=' + data[i].id + '" class="btn btn-primary">View Recipe</a>';
+                str += '</div>';
+                str += '</div>';
+                str += '</div>';
+
+            }
+            str += "</div>";
+
+            recipesdiv.innerHTML = str;
+        }
+    };
+    xmlhttp.open("GET", sstr, true);
+    xmlhttp.send();
+}
+
+function searchByCourse() {
+    var courseid = document.getElementById("course");
+    if (courseid.value == '') {
+        window.location.replace("listrecipes");
+        return;
+    }
+    var sstr = "/searchbycourse?course=";
+
+    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+    sstr += courseid.value;
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            var recipesdiv = document.getElementById("recipes");
+            var str = '<div class="row">'
+
+
+            for (var i = 0; i < data.length; i++) {
+                str += '<div class="col-sm-4 mb-3">';
+                str += '<div class="card" style="width: 18rem;">';
+                str += '<img class="card-img-top" src="static/images/';
+                str += '' + data[i].image + '" height="20%" width="20%">';
+                str += '<div class="card-body">';
+                str += ' <h5 class="card-title">' + data[i].name + '</h5>';
+                str += '<p class="card-text">A ';
+                str += '' + data[i].course + ' ' + data[i].country + ' dish ' + ' </p>';
+                str += ' <a href="recipe?id=' + data[i].id + '" class="btn btn-primary">View Recipe</a>';
+                str += '</div>';
+                str += '</div>';
+                str += '</div>';
+
+            }
+            str += "</div>";
+            recipesdiv.innerHTML = str;
+        }
+    };
+    xmlhttp.open("GET", sstr, true);
+    xmlhttp.send();
+}
