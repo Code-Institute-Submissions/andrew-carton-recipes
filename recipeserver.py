aac140538cd88e53a5efb5d1a4c24ff96c5d2661
Andrew Carton <andrew.t.carton@gmail.com>
2018-11-01 16:23:46 +0000

    Added modal activation on register button on login page

--- recipeserver.py
+++ recipeserver.py
@@ -1,4 +1,4 @@
 from recipemain import *
 
 os.environ["PORT"] = "8080"
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT')))
+app.run(debug=True, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT')))
