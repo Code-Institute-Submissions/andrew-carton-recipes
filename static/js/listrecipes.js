c64f57ea9a720e683deb0e38bcbad0c792c77b15
Andrew Carton <andrew.t.carton@gmail.com>
2018-11-04 17:00:46 +0000

    Added more comments to javascript files

--- static/js/listrecipes.js
+++ static/js/listrecipes.js
@@ -1,20 +1,34 @@
+
+// This function searches the database by allergen
 function searchByAllergen() {
     var allergenid = document.getElementById("allergen");
+
+    // If the value is empty, just redirect back to recipe page for full listing
     if (allergenid.value == '') {
         window.location.replace("listrecipes");
         return;
     }
+    // The URL with an attribute to send to backend
     var sstr = "/searchexcludeallergen?allergen=";
 
-    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+    // New AJAX request
+    var xmlhttp = new XMLHttpRequest(); 
+
+    // Append the allergen value
     sstr += allergenid.value;
+
+
     xmlhttp.onreadystatechange = function () {
+        // On success
         if (this.readyState == 4 && this.status == 200) {
+            // Deserialise the data
             var data = JSON.parse(this.responseText);
+            // Get the recipes div to add the data to
             var recipesdiv = document.getElementById("recipes");
+
             var str = '<div class="row">'
 
-
+            // Add the cards with the data
             for (var i = 0; i < data.length; i++) {
                 str += '<div class="col-sm-4 mb-3">';
                 str += '<div class="card" style="width: 18rem;">';
@@ -31,31 +45,45 @@ function searchByAllergen() {
 
             }
             str += "</div>";
+            // Set the div to the list of cards
             recipesdiv.innerHTML = str;
         }
     };
+    // AJAX GET of URL
     xmlhttp.open("GET", sstr, true);
     xmlhttp.send();
 }
 
+// This function searches by ingredient
 function searchByIngredient() {
+    // Get the ingredient
     var ingredientid = document.getElementById("ingredient");
+
+    // If empty - give full listing again
     if (ingredientid.value == '') {
         window.location.replace("listrecipes");
         return;
     }
+
+    // String for AJAX Get with attribute
     var sstr = "/searchbyingredient?ingredient=";
 
-    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+    // AJAX request
+    var xmlhttp = new XMLHttpRequest();
+    // concat value of ingredient to URL 
     sstr += ingredientid.value;
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            // On success
+
+            // deserialise JSON into data
             var data = JSON.parse(this.responseText);
+            // Get where to insert data
             var recipesdiv = document.getElementById("recipes");
 
             var str = '<div class="row">'
 
-
+            // Place data into boostrap cards
             for (var i = 0; i < data.length; i++) {
                 str += '<div class="col-sm-4 mb-3">';
                 str += '<div class="card" style="width: 18rem;">';
@@ -72,31 +100,46 @@ function searchByIngredient() {
 
             }
             str += "</div>";
-
+            // Set data in div
             recipesdiv.innerHTML = str;
         }
     };
+    // AJAX GET
     xmlhttp.open("GET", sstr, true);
     xmlhttp.send();
 }
 
+// Function to search by course
 function searchByCourse() {
+    // Get course to search for
     var courseid = document.getElementById("course");
+
+    // IF empty - give full listing again
     if (courseid.value == '') {
         window.location.replace("listrecipes");
         return;
     }
+
+    // URL with attribute to send
     var sstr = "/searchbycourse?course=";
 
-    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+    // New AJAX request
+    var xmlhttp = new XMLHttpRequest();
+    
+    // Append course search to end of URL
     sstr += courseid.value;
+
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            // On Success
+
+            // Deserialise JSON data from backend
             var data = JSON.parse(this.responseText);
+            // Get div to insert data
             var recipesdiv = document.getElementById("recipes");
             var str = '<div class="row">'
 
-
+            // Create data with cards
             for (var i = 0; i < data.length; i++) {
                 str += '<div class="col-sm-4 mb-3">';
                 str += '<div class="card" style="width: 18rem;">';
@@ -113,9 +156,11 @@ function searchByCourse() {
 
             }
             str += "</div>";
+            // Insert data into table
             recipesdiv.innerHTML = str;
         }
     };
+    // AJAX get request
     xmlhttp.open("GET", sstr, true);
     xmlhttp.send();
 }
--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,41 +1,54 @@
+
+// Adds ingredient to table
 function addIngredient() {
+    // Remove the 'remove item' 'X' after each element in table
     removeRemove();
+
+    // Get data to add ingredient
     var id = document.getElementById("ingredient");
     var aid = document.getElementById("allergen");
     var tid = document.getElementById("ingredients");
     var amid = document.getElementById("amount");
 
-
-
+    // Add the ingredient data to table from form
     tid.innerHTML += "<tr><td class=\"myfont table-padding\">" + id.value + "</td><td class=\"myfont table-padding\">" + amid.value + "</td><td class=\"myfont table-padding\">" + aid.value + "</td></tr>";
+    // Reset form items to empty
     id.value = '';
     aid.value = '';
     amid.value = '';
+
+    // Add the 'remove item' 'X' after each element in the table so they can continue to remove items
     addRemove();
 }
 
-
+// Add direction to table
 function addDirection() {
+    // Remove the 'remove item' 'X' after each element in table
     removeRemove();
+    // Get the data to add directions and the table itself
     var id = document.getElementById("direction");
-
     var tid = document.getElementById("directions");
+
+    // Get row length as current direction number to place into  table
     var rows = tid.rows;
     var dirnum = tid.rows.length;
+    
+    // Insert the direction into table
     tid.innerHTML += "<tr draggable=\"true\" class=\"column\" style=\"cursor: move;\"><td class=\"myfont table-padding\">" + dirnum++ + "</td><td class=\"myfont table-padding\">" + id.value + "</td></tr>";
 
-    var tid = document.getElementById("directions");
-    var rows = tid.rows;
-
-
-
+    // Reset value
     id.value = '';
+    // Add the callbacks for the drop-and-drag table so user can reposition the recipe directions
     addCallbacks();
+
+    // Add the 'remove item' 'X' after each element in the table so they can continue to remove items
     addRemove();
 }
 
+// The source element from where the dragging happens
 var dragSrcEl = null;
 
+// This function reorders the direction positions in the recipe directions table
 function reorder() {
     var table = document.getElementById('directions');
 
@@ -45,45 +58,55 @@ function reorder() {
     }
 }
 
+// Handle Drag Over event
 function handleDragOver(e) {
     if (e.preventDefault) {
-        e.preventDefault(); 
+        e.preventDefault();
     }
 
-    e.dataTransfer.dropEffect = 'move'; 
+    // Effect of the drag over event on the table
+    e.dataTransfer.dropEffect = 'move';
 
     return false;
 }
 
+// Handle Drag Ender event
 function handleDragEnter(e) {
     this.classList.add('over');
 
 }
 
+// Handle Drag Over event
 function handleDragOver(e) {
     if (e.preventDefault) {
-        e.preventDefault(); 
+        e.preventDefault();
     }
 
-    e.dataTransfer.dropEffect = 'move'; 
+    // Give the move drop Effect
+    e.dataTransfer.dropEffect = 'move';
 
     return false;
 
 }
 
+// Handle Drag Leave event
 function handleDragLeave(e) {
     this.classList.remove('over');
 
 }
 
+// Handle Drag End event (Release button )
 function handleDragEnd(e) {
     var cols = document.querySelectorAll('#directions .column');
     [].forEach.call(cols, function (col) {
         col.classList.remove('over');
     });
+
+    // Reorder after drag end
     reorder();
 }
 
+// Handle the drop
 function handleDrop(e) {
 
     if (e.stopPropagation) {
@@ -93,12 +116,16 @@ function handleDrop(e) {
         dragSrcEl.innerHTML = this.innerHTML;
         this.innerHTML = e.dataTransfer.getData('text/html');
     }
+    // Remove the 'remove X' items on the tables
     removeRemove();
+    // Add the 'remove X' items on the tables.
+    // This is done to refresh the 'X' on the tables when something is moved
     addRemove();
 
     return false;
 }
 
+// Handle Drag Start event
 function handleDragStart(e) {
     dragSrcEl = this;
     e.dataTransfer.effectAllowed = 'move';
@@ -106,11 +133,14 @@ function handleDragStart(e) {
 }
 
 
+// Remove the 'X items' on the recipe directions table
+// and ingredients table 
 function removeRemove() {
 
     var table = document.getElementById('directions');
 
     rows = table.rows;
+    // Iterate over the table rows and delete last cell
     for (var i = 0; i < rows.length; i++) {
         for (var j = 0; j < rows[i].cells.length; j++) {
             if (rows[i].cells[j].innerHTML == 'x') {
@@ -123,6 +153,7 @@ function removeRemove() {
     table = document.getElementById('ingredients');
 
     rows = table.rows;
+    // Iterate over the table rows and delete last cell
     for (var i = 0; i < rows.length; i++) {
         for (var j = 0; j < rows[i].cells.length; j++) {
             if (rows[i].cells[j].innerHTML == 'x') {
@@ -134,17 +165,20 @@ function removeRemove() {
 
 }
 
+// Add the remove 'X items' on the directions and ingredients tables
 function addRemove() {
 
     var table = document.getElementById('directions');
     var rows = table.rows;
+    // Iterate over the rows and insert a red 'x' in the last cell
     for (var i = 0; i < rows.length; i++) {
 
         cell = rows[i].insertCell(rows[i].cells.length);
         cell.style.color = "red";
         cell.innerHTML = "x";
 
-
+        // Create a mouseover pointer on the X when the user hovers
+        // over the small X, and add a callback to the onclick event
         cell.addEventListener('mouseover', function (e) {
             this.style.cursor = "pointer";
         }, false);
@@ -162,6 +196,7 @@ function addRemove() {
 
 
             }
+            // Reorder directions table
             reorder();
 
         }, false);
@@ -169,6 +204,8 @@ function addRemove() {
 
     var table = document.getElementById('ingredients');
     var rows = table.rows;
+    // Iterate over the rows and add a small 'X' to indicate to the user they can delete
+    // the item in the row. Add to last cell.
     for (var i = 1; i < rows.length; i++) {
 
         cell = rows[i].insertCell(rows[i].cells.length);
@@ -176,6 +213,8 @@ function addRemove() {
         cell.innerHTML = "x";
 
 
+        // Create a mouseover pointer on the X when the user hovers
+        // over the small X, and add a callback to the onclick event 
         cell.addEventListener('mouseover', function (e) {
             this.style.cursor = "pointer";
         }, false);
@@ -200,12 +239,17 @@ function addRemove() {
 
 }
 
+// This function adds the direction form to the table when 
+// the user hits the edit button
 function addDirectionForm() {
     var iform = document.getElementById("directionform");
+    // Create a form
     f = document.createElement("form");
+    // Action
     f.setAttribute("action", "javascript:addDirection()");
     f.setAttribute("method", "POST");
 
+    // Input data
     i1 = document.createElement("input");
     i1.type = "text";
     i1.id = "direction";
@@ -213,7 +257,7 @@ function addDirectionForm() {
     i1.placeholder = "Direction";
     f.appendChild(i1);
 
-
+    // Button
     i4 = document.createElement("input");
     i4.type = "submit";
     i4.className = "btn btn-primary myfont";
@@ -224,12 +268,17 @@ function addDirectionForm() {
     iform.appendChild(f);
 }
 
+// This function adds the course form to the table when 
+// the user hits the edit button
 function addCourseForm() {
+    // Form created
     var iform = document.getElementById("courseform");
     f = document.createElement("form");
+    // Action
     f.setAttribute("action", "");
     f.setAttribute("method", "POST");
 
+    // Input
     i1 = document.createElement("input");
     i1.type = "text";
     i1.id = "course"
@@ -239,12 +288,17 @@ function addCourseForm() {
     iform.appendChild(f);
 }
 
+// This function adds the country form to the table when 
+// the user hits the edit button
 function addCountryForm() {
     var iform = document.getElementById("countryform");
+    // Create form
     f = document.createElement("form");
+    // ACTION
     f.setAttribute("action", "");
     f.setAttribute("method", "POST");
 
+    // INPUT element
     i1 = document.createElement("input");
     i1.type = "text";
     i1.id = "country"
@@ -254,12 +308,18 @@ function addCountryForm() {
     iform.appendChild(f);
 }
 
+// This function adds the ingredient form to the table when 
+// the user hits the edit button
+
 function addIngredientForm() {
     var iform = document.getElementById("ingredientform");
+    // Form
     f = document.createElement("form");
+    // Action calls addIngredient() function in javascript
     f.setAttribute("action", "javascript:addIngredient()");
     f.setAttribute("method", "POST");
 
+    // Input element
     i1 = document.createElement("input");
     i1.type = "text";
     i1.id = "ingredient";
@@ -290,10 +350,13 @@ function addIngredientForm() {
     iform.appendChild(f);
 }
 
+// Add callbacks to directions table to make the drag and drop functionality
+// of the table work.
 function addCallbacks() {
     var cols = document.querySelectorAll('#directions .column');
     [].forEach.call(cols, function (col) {
 
+        // All events handled in included file above
         col.addEventListener('dragstart', handleDragStart, false);
         col.addEventListener('dragenter', handleDragEnter, false);
         col.addEventListener('dragover', handleDragOver, false);
@@ -304,18 +367,24 @@ function addCallbacks() {
     });
 }
 
+// This function makes the recipe editable
 function makeEditable() {
+    // Add the remove item 'X' to the recipe tables
     addRemove();
+    // Add callbacks to the drag and drop table
     addCallbacks();
 
+    // Add some guidance for the user
     var ed = document.getElementById("editing");
     ed.innerHTML = "<p style=\"color:red\">You can edit by deleting the entry with the small red x next to row, adding to the directions or ingredients list, or reorder the direction rows by drag and dropping</p>";
 
+    // Add the forms that can be edited
     addIngredientForm();
     addDirectionForm();
     addCourseForm();
     addCountryForm();
 
+    // Set buttons enable / disabled
     var eb = document.getElementById("editbutton");
     eb.disabled = true;
 
@@ -324,29 +393,37 @@ function makeEditable() {
 
 }
 
+// This function is called when the user hits delete recipe
+// It send an AJAX request to backend to delete the recipe
 function deleterecipe() {
 
+    // Confirm first before deleting a recipe
     if (confirm('Are you sure you want to delete this recipe?')) {
+        // Create object to serialise to JSON
         var o = new Object()
         o.id = recipeId;
-        var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+        // New AJAX POST
+        var xmlhttp = new XMLHttpRequest(); 
         xmlhttp.open("POST", "/deleterecipe");
         xmlhttp.setRequestHeader("Content-Type", "application/json");
+        // Serialise
         xmlhttp.send(JSON.stringify(o));
-        window.location.replace("listrecipes");
-
+        // Redirect back to recipe list so recipe is deleted in list
+        window.location.href = "/listrecipes";
     }
-
 }
 
+// Save the edited recipe
+// This function is called when the Save button is hit
 function saveAll() {
-
-
+    // directory position
     var dirnum = 1;
+    // object list collection
     var dirlist = [];
 
     var table = document.getElementById('directions');
 
+    // Iterate over table and append directions to list
     rows = table.rows;
     for (var i = 0; i < rows.length; i++) {
         var dirobj = new Object();
@@ -359,6 +436,8 @@ function saveAll() {
     var ingrlist = [];
     table = document.getElementById('ingredients');
 
+    // Iterate over ingredients table and append individual ingredients
+    // to list
     rows = table.rows;
     for (var i = 1; i < rows.length; i++) {
 
@@ -369,8 +448,12 @@ function saveAll() {
         ingrlist = ingrlist.concat(ingobj);
 
     }
+
+    // Create an object to serialise to JSON
     var ingrObj = new Object();
 
+    // All the data retrieved from the forms --
+    // add all data to the object
     ingrObj.name = recipeName;
     ingrObj.country = document.getElementById("country").value;
     ingrObj.course = document.getElementById("course").value;
@@ -378,14 +461,19 @@ function saveAll() {
     ingrObj.views = recipeViews;
     ingrObj.id = recipeId;
     ingrObj.image = recipeImage;
+    // Collections
     ingrObj.ingredients = ingrlist;
     ingrObj.directions = dirlist;
-    var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+    // New AJAX POST
+    var xmlhttp = new XMLHttpRequest();  
+    // Send to this backend address
     xmlhttp.open("POST", "/updaterecipe");
     xmlhttp.setRequestHeader("Content-Type", "application/json");
+    // Serialise 
     xmlhttp.send(JSON.stringify(ingrObj));
 
 
+    // Enable / Disable buttons again once saved
 
     var eb = document.getElementById("editbutton");
     eb.disabled = false;
@@ -393,7 +481,10 @@ function saveAll() {
     var sb = document.getElementById("savebutton");
     sb.disabled = true;
 
+    // Remove the 'remove X items' on tables
     removeRemove();
+
+    // Reset data
     var iform = document.getElementById("ingredientform");
     iform.innerHTML = "";
     iform = document.getElementById("directionform");
@@ -405,6 +496,7 @@ function saveAll() {
     var ed = document.getElementById("editing");
     ed.innerHTML = "";
 
+    // Redirect back to list
     window.location.href = "/listrecipes";
 
 }
